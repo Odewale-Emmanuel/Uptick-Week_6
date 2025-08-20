c3d7b168fa26a095b00b76bd466f9197310ec89c
odewale emmanuel <odebzemmygee@gmail.com>
2025-08-20 14:37:51 +0100

    ssr dashboard test 1

--- app/dashboard-ssr/page.tsx
+++ app/dashboard-ssr/page.tsx
@@ -1,27 +1,59 @@
 import UserDashboard from "@/components/user-dashboard";
 import { cookies } from "next/headers";
+import { jwtDecode } from "jwt-decode";
+import axios from "axios";
+import type { DecodedToken } from "@/types/decoded-token";
+import type { Note } from "@/types/note";
 
 export default async function Dashboard() {
   const cookiesStore = await cookies();
-  const authToken = cookiesStore.get("accessToken")?.value || null;
-  const refreshToken = cookiesStore.get("refreshToken")?.value || null;
+  const authToken = cookiesStore.get("accessToken")?.value || "";
+  const refreshToken = cookiesStore.get("refreshToken")?.value || "";
 
-  console.log("authToken", authToken);
-  console.log("refreshToken", refreshToken);
+  let notes: Note[] = [];
+  let user: DecodedToken | null = null;
+  let invalidToken = false;
+  let tokenNotFound = false;
+
+  if (!authToken) {
+    tokenNotFound = true;
+  }
+
+  try {
+    user = jwtDecode<DecodedToken>(authToken);
+  } catch (error: unknown) {
+    if (error) {
+      invalidToken = true;
+      throw error;
+    }
+  }
+
+  const fetchNotes = async () => {
+    try {
+      const response = await axios.get<Note[]>(
+        `https://uptick-week-4.onrender.com/api/note?user_id=${user?._id}`,
+        {
+          headers: { Authorization: `Bearer ${authToken}` },
+        }
+      );
+      notes = response.data;
+    } catch (error: unknown) {
+      if (error) throw error;
+      throw error;
+    }
+  };
+
+  fetchNotes();
+
   return (
-    <div>
-      <p className="flex flex-col gap-2">
-        server rendered <span>{authToken}</span> <span>{refreshToken}</span>
-      </p>
-      <UserDashboard
-        user={null}
-        authToken={null}
-        refreshToken={null}
-        invalidToken={false}
-        tokenNotFound={false}
-        notes={[]}
-        loading={false}
-      />
-    </div>
+    <UserDashboard
+      user={user}
+      authToken={authToken}
+      refreshToken={refreshToken}
+      invalidToken={invalidToken}
+      tokenNotFound={tokenNotFound}
+      notes={notes}
+      loading={false}
+    />
   );
 }
