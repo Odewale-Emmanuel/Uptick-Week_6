ecce7a631233f0e229ed4e3368fdeb83fb4bca1e
odewale emmanuel <odebzemmygee@gmail.com>
2025-08-20 16:23:34 +0100

    cookie error check

--- app/dashboard-ssr/page.tsx
+++ app/dashboard-ssr/page.tsx
@@ -7,12 +7,10 @@ import type { Note } from "@/types/note";
 
 export default async function Dashboard() {
   const cookiesStore = await cookies();
-  const authToken =
-    cookiesStore.get("accessToken")?.value ||
-    localStorage.getItem("authToken") ||
-    "";
+  const authToken = cookiesStore.get("accessToken")?.value || "";
   const refreshToken = cookiesStore.get("refreshToken")?.value || "";
 
+  console.log("cookiesStore", cookiesStore);
   console.log("authToken", authToken);
   console.log("refreshToken", refreshToken);
 
